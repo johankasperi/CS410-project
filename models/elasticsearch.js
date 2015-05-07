9552e58758000e5c174be4b0f88cd9b6d380068a
Johan Kasperi <johan@kasperi.se>
2015-05-07 02:36:51 -0500

    backend almost done

--- models/elasticsearch.js
+++ models/elasticsearch.js
@@ -5,31 +5,116 @@ var client = new elasticsearch.Client({
   log: 'trace'
 });
 
-var query = function(pageNum, userQuery, callback) {
-	client.search({
-	  index: 'kasperi2_index',
-	  from: (pageNum - 1) * 20,
-	  size: 20,
-	  body_stopstem: {
-		    match : {
-		        message : {
-		            query: userQuery,
-		            operator: "and"
-		        }
-		    }
-	  	}
-	}, function (error, response) {
-	  if (error) {
-	    // handle error
-	    return callback(error, null);
-	  }
+var query = function(req, callback) {
+	var pageNum = req.param('page', 1);
+	var docPerPage = 2;
 
-	  callback(null, {
-	    results: response.hits.hits,
-	    page: pageNum,
-	    pages: Math.ceil(response.hits.total / 20)
-	  });
-	});
+	buildQuery(req, function(query) {
+		client.search({
+			  index: 'kasperi2_index',
+			  from: (pageNum - 1) * docPerPage,
+			  size: docPerPage,
+			  body: query
+			}, function (error, response) {
+			  if (error) {
+			    return callback(error, null);
+			  }
+
+			  return callback(null, {
+			    results: response.hits.hits,
+			    page: pageNum,
+			    pages: Math.ceil(response.hits.total / docPerPage)
+			  });
+		});
+	})
+}
+
+var buildQuery = function(req, callback) {
+	var queryType = req.param('query_type', 1);
+	var userQuery = req.param('search_query');
+	var similarity = req.param('similarity', 'BM25');
+
+	var selectedAlgorithm = createAlgorithm(req);
+	var titleField = "title_"+selectedAlgorithm+"_"+similarity;
+	var bodyField = "body_"+selectedAlgorithm+"_"+similarity;
+
+	// If "multi_match" is selected
+	if(queryType == 1) {
+		var titleboost = req.param('titleboost', 1);
+		var bodyboost = req.param('bodyboost', 1)
+		var query = {
+			query: {
+				multi_match : {
+				    query: userQuery,
+		    		fields: [ titleField+"^"+titleboost, bodyField+"^"+bodyboost ],
+				}
+	  		}
+		}
+		if(selectedAlgorithm !== "raw") {
+			query.query.multi_match["analyzer"] = selectedAlgorithm+"_analyzer"
+		}
+	}
+
+	// If "more like this" is selected
+	else if(queryType == 2) {
+		var boostTerms = req.param("boost_terms", 0);
+		var query = {
+			query: {
+			    more_like_this : {
+			        fields: [titleField, bodyField],
+			        like_text: userQuery,
+			        boost_terms: boostTerms,
+			        min_term_freq : 1,
+			    }
+	  		}
+		}
+		if(selectedAlgorithm !== "raw") {
+			query.query.more_like_this["analyzer"] = selectedAlgorithm+"_analyzer"
+		}
+	}
+
+	// If "common" is selected
+	else if(queryType == 3) {
+		var cutOff = req.param("cutoff_frequency", 0.001);
+		var field = bodyField;
+		var query = {
+			query: {
+			  	common: {
+			  	}
+			}
+		}
+		query.query.common[bodyField] = {
+	        query: userQuery,
+	        cutoff_frequency: cutOff,
+	        low_freq_operator: "and"
+    	}
+    	if(selectedAlgorithm !== "raw") {
+			query.query.common[bodyField]["analyzer"] = selectedAlgorithm+"_analyzer"
+		}
+	}
+	return callback(query)
+}
+
+var createAlgorithm = function(req) {
+	var stop = req.param('stop', 0);
+	var stem = req.param('stem', 0);
+	var lowercase = req.param('lowercase', 0);
+
+	var selectedAlgorithm = "";
+	if(stop == 1) {
+		selectedAlgorithm += "stop";
+	}
+	if(stem == 1) {
+		selectedAlgorithm += "stem";
+	}
+	if(lowercase == 1) {
+		selectedAlgorithm += "lowercase";
+	}
+	if(selectedAlgorithm.length < 1) {
+		selectedAlgorithm += "raw";
+	}
+
+	return selectedAlgorithm;
 }
 
 module.exports = query;
--- routes/routing.js
+++ routes/routing.js
@@ -2,13 +2,12 @@ var express = require('express');
 var router = express.Router();
 var elasticsearch = require('../models/elasticsearch');
 
-router.get('/', function(req, res) {
-  var pageNum = req.param('page', 1);
-  var userQuery = req.param('search_query');
+router.get('/query', function(req, res) {
+  var userQuery = req.param("search_query");
   if(!userQuery) {
   	res.send("Please include query");
   }
-  elasticsearch(pageNum, userQuery, function(error, response) {
+  elasticsearch(req, function(error, response) {
   	if(error) {
   		res.send(error);
   	}
