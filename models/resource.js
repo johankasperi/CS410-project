805d2e51098eefa8e734ee9ff6eb8bb04a6002f2
Johan Kasperi <johan@kasperi.se>
2015-05-11 17:22:35 -0500

    bug fixes

--- models/resource.js
+++ models/resource.js
@@ -11,18 +11,18 @@ var resources = [
 		id: 2,
 		name: "Stop word filtering",
 		content: "Words like: <i>I, the, of, my, it, to, from</i> are called stop words since they are very high frequency words that do not contain content information. They are grammatically required for connecting sentences. Since these words do not contain any topical information, they are often removed as a preprocessing step in text analysis. Not only are these (usually) useless words ignored, but having less data can mean that algorithms run faster!",
-		resource: "http://en.wikipedia.org/wiki/Stemming"
+		resource: "http://en.wikipedia.org/wiki/Stop_words"
 	},
 	{
 		id: 3,
 		name: "Lowercase filtering",
-		content: "",
+		content: "Lowercase filtering means that all captial letters in the words in the query and in the document will be transform to lowercase letters.",
 		resource: "http://www.elastic.co/guide/en/elasticsearch/reference/1.3/analysis-lowercase-tokenfilter.html"
 	},
 	{
 		id: 4,
 		name: "Okapi BM25 similarity function",
-		content: "",
+		content: "The BM stands for “Best Matching”. As the name suggests, BM25 is a ranking function used by search engines to rank matching documents according to their relevance to a given search query. It is based on the probabilistic retrieval framework which makes an estimation of the probability of finding if a document is relevant to a query.",
 		resource: "http://en.wikipedia.org/wiki/Okapi_BM25"
 	},
 	{
